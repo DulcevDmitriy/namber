c3a7f658fe23447dcbddb426c96ee8be1d183643
Dulcev Dmitriy <ddvvvvvv@yandex.ru>
2022-12-08 14:01:51 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int initialAmount = 100;
+        int addAmount = 1100;
+
+        int bonus;
+        if (addAmount > 1000) {
+            bonus = addAmount / 100;
+        } else {
+            bonus = 0;
+        }
+        int finalAmount = initialAmount + addAmount + bonus;
+        System.out.println("Бонусов" + bonus);
+        System.out.println("Итоговый счет" + finalAmount);
     }
 }
+
